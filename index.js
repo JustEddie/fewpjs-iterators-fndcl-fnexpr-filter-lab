d73e5f662ccbd265af2500751ae60fc8d18ca444
JustEddie <skdpwls830@gmail.com>
2022-07-16 22:34:20 +1000

    commit tree 8e0c7bc7f86ad7ba8aa486f2f60a58b7fd00328e

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(arr,str) {
+ 
+    return arr.filter(x=> {return x.toLowerCase()===str.toLowerCase()});
+}
+
+
+
+function fuzzyMatch(arr,str) {
+    return arr.filter(x=> {return x.startsWith(str)});
+}
+
+function matchName(arr,str) {
+    return arr.filter(x => x.name ===str);
+}
